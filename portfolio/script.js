10905fe34de363464a4ae086ada7f5542827a841
Katie Chai <kchai@unc.edu>
2023-05-28 14:27:10 -0400

    nav bar fixes

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,6 +1,6 @@
-$('.navTrigger').click(function () {
-  $(this).toggleClass('active');
-  console.log("Clicked menu");
-  $("#mainListDiv").toggleClass("show_list");
-  $("#mainListDiv").fadeIn();
-});
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navBarLinks = document.getElementsByClassName('nav-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navBarLinks.classList.toggle('active')
+})
